e0cebe66bc29aa3e66a9c8956f9062a83427803f
Long-Giang Vu <giangvl.cs@gmail.com>
2025-06-04 23:17:16 -0700

    feat: update data path

--- sanity_check_SFCHD.sh
+++ sanity_check_SFCHD.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py --data-path data/miniSFCHD/yolo --image-folder data/miniSFCHD/images --N 10 --num-classes 7 --batch-size 2 --num-epochs 100 --lr 0.001
+python train.py --data-path ../data/miniSFCHD/yolo --image-folder ../data/miniSFCHD/images --N 10 --num-classes 7 --batch-size 2 --num-epochs 100 --lr 0.001
