79d52522916e30a59f5179976aea20b2ae9c4fb9
Giang Vu Long <longgiang2015bn@gmail.com>
2025-06-10 04:22:08 +0700

    change logging setting

--- modules/loss.py
+++ modules/loss.py
@@ -182,9 +182,9 @@ class SetCriterion(nn.Module):
 
         # Retrieve the matching between the outputs of the last layer and the targets
         indices = self.matcher(outputs_without_aux, targets)
-        idx_sorted = torch.argsort(indices[1])
+        idx_sorted = torch.argsort(indices[0][1])
 
-        logger.info(f"Indices: {indices[1][idx_sorted]}, {indices[0][idx_sorted]}")
+        logger.info(f"Indices: {indices[0][1][idx_sorted]}, {indices[0][0][idx_sorted]}")
         # Compute the average number of target boxes accross all nodes, for normalization purposes
         num_boxes = sum(len(t["labels"]) for t in targets)
         num_boxes = torch.as_tensor([num_boxes], dtype=torch.float, device=next(iter(outputs.values())).device)
